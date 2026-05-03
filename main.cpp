7993363e3ce81ee67a28227a52814205f94856dd
Ranbon-Kafa <rname8064@gmail.com>
2026-05-03 15:50:21 +0300

    kod değişikliği

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <thread>
 
 using namespace std;
 
@@ -27,11 +28,24 @@ int main() {
         case '*':
             cout << sayi1 << " * " << sayi2 << " = " << sayi1 * sayi2 << endl;
             break;
-            
-        case '/':
-            cout << sayi1 << " / " << sayi2 << " = " << sayi1 / sayi2 << endl;
-            break;
-    }
 
+        case '/':
+            if (sayi2 == 0) {
+                cout << "Hata: Sayi2 sifira bolunemez!" << endl;
+                cout << "Ikinci sayiyi giriniz: ";
+                cin >> sayi2;
+                if (sayi2 == 0) {
+                    cout << "Hata: Sayi2 sifira bolunemez!" << endl;
+                } else {
+                    cout << sayi1 << " / " << sayi2 << " = " << sayi1 / sayi2 << endl;
+                }
+            } else {
+                cout << sayi1 << " / " << sayi2 << " = " << sayi1 / sayi2 << endl;
+            }
+            break;
+            default:
+                cout << "Gecersiz islem secimi!" << endl;
+    }
+    std::this_thread::sleep_for(std::chrono::seconds(3));
     return 0;
 }
